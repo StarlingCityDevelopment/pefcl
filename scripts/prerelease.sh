5c050dd34d7f5cb4a4d815d0ca181d13da9e3b73
Glitch <10230512-GlitchX_x@users.noreply.gitlab.com>
2025-02-10 11:31:24 -0500

    chore(release scripts): remove media folder from root web folder

--- scripts/prerelease.sh
+++ scripts/prerelease.sh
@@ -6,7 +6,7 @@ RESOURCE="pefcl"
 echo "$PREFIX Creating pre-release"
 ## Create temporary folder and move files into it. Keeping same structure according to fxmanifest.lua
 mkdir -p ./temp/$RESOURCE/src/client
-mkdir -p ./temp/$RESOURCE/web/media
+mkdir -p ./temp/$RESOURCE/web
 cp LICENSE README.md config.json import.sql fxmanifest.lua ./temp/$RESOURCE
 cp -r ./src/dist ./temp/$RESOURCE/src/dist # Copy resource files
 cp -r ./src/client/lua/ ./temp/$RESOURCE/src/client/lua # Copy Lua files
