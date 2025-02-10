97bf7b00a625d33ead8f1140a855668f26721756
Glitch <10230512-GlitchX_x@users.noreply.gitlab.com>
2025-02-10 11:56:09 -0500

    chore(fxmanifest): bump version to 2.0.1

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 
 description "Financing resource. Accounts, Cash, Invoices, Transactions."
 author "Project Error"
-version '1.0.0'
+version '2.0.1'
 repository 'https://github.com/project-error/fivem-react-boilerplate-lua'
 
 lua54 'yes'
